c0ad5f270710b682ef2020da1b2b9d47c5432dcc
EmmanuelBahena <mannybahena04@gmail.com>
2023-05-01 18:47:01 -0500

    make Users controller Full Stack

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,39 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+    render template: "users/new"
+  end
+
+
   def create
-    user = User.new(
-      name: params[:name],
-      email: params[:email],
-      password: params[:password],
-      password_confirmation: params[:password_confirmation]
+    @user = User.new(
+      name: params[:user][:name],
+      email: params[:user][:email],
+      password: params[:user][:password],
+      password_confirmation: params[:user][:password_confirmation],
     )
-    if user.save
+    if @user.save
       render json: { message: "User created successfully" }, status: :created
     else
-      render json: { errors: user.errors.full_messages }, status: :bad_request
+      render :new, status: :unprocessable_entity
     end
+  end 
+
+  def edit
+    @game = Game.find_by(id: params[:id])
+    render :edit
   end
-end
+
+  def update
+    @game = Game.find_by(id: params[:id])
+    @game.update(
+      name: params[:user][:name],
+      email: params[:user][:email],
+      password: params[:user][:password],
+      password_confirmation: params[:user][:password_confirmation],
+    )
+    redirect_to "/games"
+  end
+
+end 
+    
