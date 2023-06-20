a17fb409be94f47fcf11182ac88c1955d014153a
EmmanuelBahena <mannybahena04@gmail.com>
2023-06-19 20:50:02 -0500

    edit signup from fullstack for API

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,39 +1,16 @@
 class UsersController < ApplicationController
-  def new
-    @user = User.new
-    render template: "users/new"
-  end
-
-
   def create
-    @user = User.new(
-      name: params[:user][:name],
-      email: params[:user][:email],
-      password: params[:user][:password],
-      password_confirmation: params[:user][:password_confirmation],
+    user = User.new(
+      name: params[:name],
+      email: params[:email],
+      password: params[:password],
+      password_confirmation: params[:password_confirmation]
     )
-    if @user.save
-      redirect_to "/games"
-      # render json: { message: "User created successfully" }, status: :created
+    if user.save
+      render json: { message: "User created successfully" }, status: :created
     else
-      render :new, status: :unprocessable_entity
+      render json: { errors: user.errors.full_messages }, status: :bad_request
     end
-  end 
-
-  def edit
-    @game = Game.find_by(id: params[:id])
-    render :edit
-  end
-
-  def update
-    @game = Game.find_by(id: params[:id])
-    @game.update(
-      name: params[:user][:name],
-      email: params[:user][:email],
-      password: params[:user][:password],
-      password_confirmation: params[:user][:password_confirmation],
-    )
-    
   end
 
 end 
