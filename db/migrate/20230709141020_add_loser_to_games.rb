0822795467b08bd2a3784e14134e4160a6b402d1
EmmanuelBahena <mannybahena04@gmail.com>
2023-07-09 09:12:02 -0500

    add loser to games table

---
+++ db/migrate/20230709141020_add_loser_to_games.rb
@@ -0,0 +1,5 @@
+class AddLoserToGames < ActiveRecord::Migration[7.0]
+  def change
+    add_column :games, :loser, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_09_140619) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_09_141020) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,6 +30,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_09_140619) do
     t.datetime "updated_at", null: false
     t.string "picture"
     t.integer "number_of_moves"
+    t.string "loser"
   end
 
   create_table "users", force: :cascade do |t|
