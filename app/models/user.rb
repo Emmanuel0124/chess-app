82f3566c440f16db551d336667126aaaf2ffc20a
EmmanuelBahena <mannybahena04@gmail.com>
2023-07-19 21:09:58 -0500

    edit associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true, uniqueness: true
 
-  has_many :games
-  has_many :comments, through: :games
+  has_many :comments
+  has_many :games, through: :comments
 end
